8eef89e916341ffaa4a183a0260a584dcbde7b69
Kamas Lau <kamaslau@outlook.com>
2024-04-09 09:54:15 +0800

    refactor: adjust config sequences for better clearency

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -1,28 +1,37 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from "vitepress";
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "Kamas' Manual",
-  description: "How-to instructions from personal experiences of product designing, programming and DevOps skills.",
+  description:
+    "How-to instructions from personal experiences of product designing, programming and DevOps skills.",
+  lastUpdated: true,
+  markdown: {
+    lineNumbers: true,
+  },
+  sitemap: {
+    hostname: "https://devea-translations.kamaslau.com",
+  },
+  srcDir: "./src",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: "Home", link: "/" },
+      { text: "Examples", link: "/markdown-examples" },
     ],
 
     sidebar: [
       {
-        text: 'Examples',
+        text: "Examples",
         items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
-        ]
-      }
+          { text: "Markdown Examples", link: "/markdown-examples" },
+          { text: "Runtime API Examples", link: "/api-examples" },
+        ],
+      },
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
-  }
-})
+      { icon: "github", link: "https://github.com/vuejs/vitepress" },
+    ],
+  },
+});
